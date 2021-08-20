591d9456ed3caa948f39b15a11893cbc1aacdd04
Neylan-Body <neylan2011@gmail.com>
2021-08-20 06:05:58 -0300

    tela de roda de conversa funcional

--- resources/views/roda_conversa.blade.php
+++ resources/views/roda_conversa.blade.php
@@ -7,30 +7,158 @@
         <div class="col-md-12">
             <!-- /.box -->
             <!-- general form elements disabled -->
-            <div class="row">
-                <div class='col-md-4'>
-                    <!-- small box -->
-                    <div class='small-box bg-white'>
-                        <div class='inner'>
-                            <h4><strong>Conversa com Psicologo</strong></h4>
-                            <strong>Universidade Estadual do Sudoeste da Bahia</strong>
-                            <img src="{{ asset('img/saudemental1.jpeg')}}" alt="" srcset=""><br>
-                            
+            <div class="card">
+                <div class="card-header p-2">
+                    <ul class="nav nav-pills">
+                    <li class="nav-item"><a class="nav-link active" href="#aovivo" data-toggle="tab">Ao vivo</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#agendados" data-toggle="tab">Agendados</a></li>
+                    </ul>
+                </div>
+                <div class="card-body">
+                    <div class="tab-content">
+                        <div class="tab-pane" id="agendados">
+                            <!-- Post -->
+                            <div class="post">
+                                <div class="row">
+                                <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com Psicologo</strong></h4><i></i>
+                                                <strong>Universidade Estadual do Sudoeste da Bahia</strong>
+                                                <img src="{{ asset('img/saudemental7.jpeg')}}" alt="" srcset=""><br>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com Psicologo</strong></h4>
+                                                <strong>Universidade Federal do Reconcavo da Bahia</strong>
+                                                <img src="{{ asset('img/saudemental8.jpeg')}}" alt="" srcset=""><br>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com estagiario de Psicologia</strong></h4>
+                                                <strong>Universidade Federal do Sul da Bahia</strong>
+                                                <img src="{{ asset('img/saudemental9.png')}}" alt="" srcset=""><br>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com estagiario de Psicologia</strong></h4>
+                                                <strong>Universidade Catolica de Salvador</strong>
+                                                <img src="{{ asset('img/saudemental10.jpeg')}}" alt="" srcset=""><br>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com estagiario de Psicologia</strong></h4>
+                                                <strong>Universidade Estadual de Santa Cruz</strong>
+                                                <img src="{{ asset('img/saudemental11.png')}}" alt="" srcset=""><br>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com estagiario de Psicologia</strong></h4>
+                                                <strong>Universidade Estadual de Feira de Santana</strong>
+                                                <img src="{{ asset('img/saudemental12.jpeg')}}" alt="" srcset=""><br>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane active" id="aovivo">
+                            <!-- Post -->
+                            <div class="post">
+                                <div class="row">
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com Psicologo</strong></h4><i></i>
+                                                <strong>Universidade Estadual do Sudoeste da Bahia</strong>
+                                                <img src="{{ asset('img/saudemental1.jpeg')}}" alt="" srcset=""><br>
+                                                <img src="{{ asset('img/live-icon.png')}}" style="width: 5em;" srcset="">                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com Psicologo</strong></h4>
+                                                <strong>Universidade Federal do Reconcavo da Bahia</strong>
+                                                <img src="{{ asset('img/saudemental2.jpeg')}}" alt="" srcset=""><br>
+                                                <img src="{{ asset('img/live-icon.png')}}" style="width: 5em;" srcset="">                                                  
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com estagiario de Psicologia</strong></h4>
+                                                <strong>Universidade Federal do Sul da Bahia</strong>
+                                                <img src="{{ asset('img/saudemental3.png')}}" alt="" srcset=""><br>
+                                                <img src="{{ asset('img/live-icon.png')}}" style="width: 5em;" srcset="">                                                  
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com estagiario de Psicologia</strong></h4>
+                                                <strong>Universidade Catolica de Salvador</strong>
+                                                <img src="{{ asset('img/saudemental4.jpeg')}}" alt="" srcset=""><br>
+                                                <img src="{{ asset('img/live-icon.png')}}" style="width: 5em;" srcset="">                                                  
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com estagiario de Psicologia</strong></h4>
+                                                <strong>Universidade Estadual de Santa Cruz</strong>
+                                                <img src="{{ asset('img/saudemental5.jpeg')}}" alt="" srcset=""><br>
+                                                <img src="{{ asset('img/live-icon.png')}}" style="width: 5em;" srcset="">                                                  
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class='col-md-4'>
+                                        <!-- small box -->
+                                        <div class='small-box bg-white'>
+                                            <div class='inner'>
+                                                <h4><strong>Conversa com estagiario de Psicologia</strong></h4>
+                                                <strong>Universidade Estadual de Feira de Santana</strong>
+                                                <img src="{{ asset('img/saudemental6.jpeg')}}" alt="" srcset=""><br>
+                                                <img src="{{ asset('img/live-icon.png')}}" style="width: 5em;" srcset="">                                                  
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class='col-md-4'>
-                    <!-- small box -->
-                    <div class='small-box bg-white'>
-                        <div class='inner'>
-                            <h4><strong>Conversa com Psicologo</strong></h4>
-                            <strong>Universidade Federal do Reconcavo da Bahia</strong>
-                            <img src="{{ asset('img/saudemental2.jpeg')}}" alt="" srcset=""><br>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </div>    
         </div>
     </div>
 @endsection
